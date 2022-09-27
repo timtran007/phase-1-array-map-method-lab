3512c5a65f34022b05cd74fa6aeccdfd97b40e58
Tim <timtran007@gmail.com>
2022-09-27 01:49:53 -0500

    commit tree 15ca784d00432bc6ff7611cf359916ec56be7158

--- index.js
+++ index.js
@@ -12,5 +12,9 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((element) =>
+  element.split(' ').map((word) =>
+    word[0].toUpperCase() + word.slice(1)
+    ).join(' ')
+  )
 }
